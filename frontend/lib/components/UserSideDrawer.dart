c1a5e8a4b6ef6d4fa005eba8b12234d279f55acc
Aryan Dhawan <aryansdhawan@gmail.com>
2024-03-14 23:18:40 -0400

    (1/n) Working on Frontend Backend Connection

---
+++ frontend/lib/components/UserSideDrawer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserSidesDrawer extends StatefulWidget {
+  const UserSidesDrawer({super.key});
+
+  @override
+  State<UserSidesDrawer> createState() => _UserSidesDrawerState();
+}
+
+class _UserSidesDrawerState extends State<UserSidesDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
